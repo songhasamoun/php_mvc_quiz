424265806fbfc86bb745e67d3bc189646ee7fbb4
Samoun.Songha <samoun.songha@PNC.LAN>
2019-03-25 11:18:49 +0700

    [These are view users file]

---
+++ models/m_user.php
@@ -0,0 +1,47 @@
+<?php 
+function validateLogin() {
+    include 'connection.php';
+    if(isset($_POST['but_submit'])){
+
+        $uname = mysqli_real_escape_string($connection,$_POST['txt_uname']);
+        $password = mysqli_real_escape_string($connection,$_POST['txt_pwd']);
+    
+        if ($uname != "" && $password != ""){
+    
+            $sql_query = "select count(*) as cntUser from user where username='".$uname."' and password='".$password."'";
+            $result = mysqli_query($connection,$sql_query);
+            $row = mysqli_fetch_array($result);
+    
+            $count = $row['cntUser'];
+    
+            if($count > 0){
+                $_SESSION['uname'] = $uname;
+                header('location:index.php?action=view');
+            }else{
+                header('location:index.php?action=login');
+            }
+        }
+    }
+}
+
+function m_insert_user() {
+    if(isset($_POST['btn-addUser'])) {
+        $username = $_POST['username'];
+        $name = $_POST['name'];
+        $getPassword = $_POST['password'];
+        $password = md5($getPassword);
+        $get_verify_password = $_POST['verify_password'];
+        $verify = md5($get_verify_password);
+        if($password == $verify) {
+            $query = "INSERT INTO user (username, name, password)
+            VALUES ('$username', '$name', '$password')";
+            include 'connection.php';
+            $result = mysqli_query($connection, $query);
+            if($result) {
+                return $result;
+            }else {
+                return "Can't Insert Into Database";
+            }
+        }
+    }
+}
